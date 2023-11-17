e4f661563ded41230dedfe247b752e3ea5cbc25c
ADITYAJOSHI1222 <91424095+ADITYAJOSHI1222@users.noreply.github.com>
2023-11-17 18:38:20 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, My name is aditya'
+    return 'Hello, My name is aditya. THis is Final Deploy'
 
 if __name__ == '__main__':
     app.run()
